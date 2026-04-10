1e341c6bad831e5e066c216236ca2cabdd30051a
Paweł Tomczyk <pawel.tomczyk.informatykk@pm.me>
2026-04-10 02:10:20 +0200

    clang and setup minor

--- Setup.sh
+++ Setup.sh
@@ -1,1 +1,6 @@
 ./Engine/ThirdParty/shaderc/utils/git-sync-deps
+
+# On ubuntu to build glfw (https://www.glfw.org/docs/latest/compile_guide.html#compile_deps_wayland)
+# sudo apt install libwayland-dev libxkbcommon-dev xorg-dev
+
+# Get vulkan sdk (https://vulkan.lunarg.com/doc/sdk/1.4.341.1/windows/getting_started.html)
